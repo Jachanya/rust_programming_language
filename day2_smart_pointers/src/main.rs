9707bf9853077832b8e9172a8739e3fb7a113e2f
jachan <johnachanya9@gmail.com>
2023-03-09 12:19:27 +0100

    Completed half of the smart pointer section

--- day2_smart_pointers/src/main.rs
+++ day2_smart_pointers/src/main.rs
@@ -2,6 +2,33 @@
  */
 
 /* Smart pointers are structs that implement Deref and Drop trait */
+/* The most strait forward smart pointer is Box<T>, this is like new 
+keyword in c++ and malloc in c. It lets you store data in the heap */
+
+/* My current understanding: Box<T> is currently the pointer to the type T
+and Box::new(T) the address or the dereferencing of the type T  */
+
+#[derive(Debug)]
+struct Payment {
+    name: String,
+    amount: i32
+}
+
+#[derive(Debug)]
+enum LinkedList {
+    Node(Box<LinkedList>, Payment),
+    Nil
+}
+
 fn main() {
-    println!("Hello, world!");
+    let payment1 = Payment { name: String::from("John Achanya"), amount: 50000};
+    let payment2 = Payment { name: String::from("Joe Achanya"), amount: 100000};
+    let node = LinkedList::Node(Box::new(LinkedList::Node(Box::new(LinkedList::Nil), payment1)), payment2);
+
+
+    let x = 5;
+    let y: Box<i32> = Box::new(x);
+
+    println!("y = {:#?}", y);
+    println!("b = {:#?}", node);
 }
---
+++ day2_smart_pointers/target/debug/day2_smart_pointers.d
@@ -0,0 +1,1 @@
+C:\Users\johna\Documents\rust_programming_language\day2_smart_pointers\target\debug\day2_smart_pointers.exe: C:\Users\johna\Documents\rust_programming_language\day2_smart_pointers\src\main.rs
