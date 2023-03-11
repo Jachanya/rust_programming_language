e8fc299afdaa4959164a61790630b8ea5dad8fd1
jachan <johnachanya9@gmail.com>
2023-03-11 05:36:25 +0100

    final project from rust programming book

---
+++ day4_web_server/src/main.rs
@@ -0,0 +1,11 @@
+/* Working on a webserver using rust programming */
+// 1. Learn about tcp and http connection
+// 2. Listen for TCP connections on socket
+// 3. parse a small number of HTTP requests
+// 4. create a proper HTTP response
+// 5. improve throughput of our server with thread pool
+
+
+fn main() {
+    println!("Hello, world!");
+}
