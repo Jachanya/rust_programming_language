38909509dc070f9c16b1cb855dd5be3d7e34d26d
jachan <johnachanya9@gmail.com>
2023-03-11 12:04:05 +0100

    completed async web server

--- day4_web_server/src/main.rs
+++ day4_web_server/src/main.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 /* Working on a webserver using rust programming */
 // 1. Learn about tcp and http connection
 // 2. Listen for TCP connections on socket
@@ -6,6 +8,14 @@
 // 5. improve throughput of our server with thread pool
 
 
+/* A webserver is guided by two web protocols which are the request-response
+protocol. The client initiates the request and the server responds */
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established");
+    }
 }
