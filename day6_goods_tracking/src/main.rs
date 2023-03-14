ca35a901f7dfd5390c792bd624ebfa43f3c2eeb2
jachan <johnachanya9@gmail.com>
2023-03-14 10:33:49 +0100

    Starting a project on p2p communication

--- day6_goods_tracking/src/main.rs
+++ day6_goods_tracking/src/main.rs
@@ -1,8 +1,13 @@
 use day6_goods_tracking::consumers;
-use futures::prelude::*;
-use libp2p::swarm::{keep_alive, NetworkBehaviour, Swarm, SwarmEvent};
-use libp2p::{identity, ping, Multiaddr, PeerId};
+use async_std::io;
+use futures::{prelude::*, select};
+use libp2p::{
+    gossipsub, identity, mdns, swarm::NetworkBehaviour, swarm::SwarmEvent, PeerId, Swarm,
+};
+use std::collections::hash_map::DefaultHasher;
 use std::error::Error;
+use std::hash::{Hash, Hasher};
+use std::time::Duration;
 /// A system to build goods and food tracking
 /// 
 /// Important actors: 
@@ -12,56 +17,100 @@ use std::error::Error;
 /// 
 /// The Merchants and the sellers commumicate using a P2P connections
 
+
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-
-    
-    let consumer1 = consumers::Consumer::new(
-        1234456,
-        "John".to_owned(), 
-        "Achanya".to_owned(),
-        "Behind yoruba mosque".to_owned());
-
-    let consumer1_name = consumer1.get_name();
-
-    println!("Hello, world!: {}", consumer1_name);
+    // Create a random PeerId
     let local_key = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(local_key.public());
-    println!("Local peer id: {local_peer_id:?}");
+    println!("Local peer id: {local_peer_id}");
 
-    let transport = libp2p::development_transport(local_key).await?;
+    // Set up an encrypted DNS-enabled TCP Transport over the Mplex protocol.
+    let transport = libp2p::development_transport(local_key.clone()).await?;
 
-    let behaviour = Behaviour::default();
+    // We create a custom network behaviour that combines Gossipsub and Mdns.
+    #[derive(NetworkBehaviour)]
+    struct MyBehaviour {
+        gossipsub: gossipsub::Behaviour,
+        mdns: mdns::async_io::Behaviour,
+    }
 
-    let mut swarm = Swarm::with_async_std_executor(transport, behaviour, local_peer_id);
+    // To content-address message, we can take the hash of message and use it as an ID.
+    let message_id_fn = |message: &gossipsub::Message| {
+        let mut s = DefaultHasher::new();
+        message.data.hash(&mut s);
+        gossipsub::MessageId::from(s.finish().to_string())
+    };
 
-    // Tell the swarm to listen on all interfaces and a random, OS-assigned
-    // port.
+    // Set a custom gossipsub configuration
+    let gossipsub_config = gossipsub::ConfigBuilder::default()
+        .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+        .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+        .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+        .build()
+        .expect("Valid config");
+
+    // build a gossipsub network behaviour
+    let mut gossipsub = gossipsub::Behaviour::new(
+        gossipsub::MessageAuthenticity::Signed(local_key),
+        gossipsub_config,
+    )
+    .expect("Correct configuration");
+
+    // Create a Gossipsub topic
+    let topic = gossipsub::IdentTopic::new("test-net");
+
+    // subscribes to our topic
+    gossipsub.subscribe(&topic)?;
+
+    // Create a Swarm to manage peers and events
+    let mut swarm = {
+        let mdns = mdns::async_io::Behaviour::new(mdns::Config::default(), local_peer_id)?;
+        let behaviour = MyBehaviour { gossipsub, mdns };
+        Swarm::with_async_std_executor(transport, behaviour, local_peer_id)
+    };
+
+    // Read full lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines().fuse();
+
+    // Listen on all interfaces and whatever port the OS assigns
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    // Dial the peer identified by the multi-address given as the second
-    // command-line argument, if any.
-    if let Some(addr) = std::env::args().nth(1) {
-        let remote: Multiaddr = addr.parse()?;
-        swarm.dial(remote)?;
-        println!("Dialed {addr}")
-    }
+    println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
 
+    // Kick it off
     loop {
-        match swarm.select_next_some().await {
-            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
-            SwarmEvent::Behaviour(event) => println!("{event:?}"),
-            _ => {}
+        select! {
+            line = stdin.select_next_some() => {
+                if let Err(e) = swarm
+                    .behaviour_mut().gossipsub
+                    .publish(topic.clone(), line.expect("Stdin not to close").as_bytes()) {
+                    println!("Publish error: {e:?}");
+                }
+            },
+            event = swarm.select_next_some() => match event {
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discovered a new peer: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+                    }
+                },
+                SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discover peer has expired: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
+                    }
+                },
+                SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
+                    propagation_source: peer_id,
+                    message_id: id,
+                    message,
+                })) => println!(
+                        "Got message: '{}' with id: {id} from peer: {peer_id}",
+                        String::from_utf8_lossy(&message.data),
+                    ),
+                _ => {}
+            }
         }
     }
-}
-
-/// Our network behaviour.
-///
-/// For illustrative purposes, this includes the [`KeepAlive`](behaviour::KeepAlive) behaviour so a continuous sequence of
-/// pings can be observed.
-#[derive(NetworkBehaviour, Default)]
-struct Behaviour {
-    keep_alive: keep_alive::Behaviour,
-    ping: ping::Behaviour,
 }
