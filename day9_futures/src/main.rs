37f327d9955e74077e9b2f4cae51d8c6e17dcd94
jachan <johnachanya9@gmail.com>
2023-03-16 18:10:35 +0100

    watch 50 mins of async/await jon Gjengset

--- day9_futures/src/main.rs
+++ day9_futures/src/main.rs
@@ -15,6 +15,22 @@
 // Futures is simply a concept
 // async fn download(url: Url) -> Result { .. }
 // .await the execution is suspended
+
+
+// Waiting on multiple features
+// select! {
+//     stream <- network.await => {
+
+//     }
+
+//     line <- terminal.await => {
+
+//     }
+// }
+// select gives us like an octopus to branch different operations
+// that can be awaited. Async await describe under what circumstances code 
+// can make progress and under what circumstances code can yield.
+
 fn main() {
     println!("Hello, world!");
 }
